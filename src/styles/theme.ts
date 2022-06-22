1ec31da2fb552c28ae63ca75e2b44d9f9e8a55d2
henrique dantas <henrique@hotmail.ph>
2022-06-21 22:33:44 -0300

    config theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,8 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
-  colors: {
-    orange: {
-      "100": "#FFAB2E",
-    },
-  },
   fonts: {
-    heading: "Sf-Regular",
-    body: "Sf-Regular",
+    body: "Montserrat",
   },
   styles: {
     global: {
